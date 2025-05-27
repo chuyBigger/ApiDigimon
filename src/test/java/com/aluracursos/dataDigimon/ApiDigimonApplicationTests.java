7a876a1261daeb611cf632075bc1d42cc2f9fffa
Jesus Medina Casas <chuytec15@gmail.com>
2025-05-27 00:17:02 -0600

    api_digimon

---
+++ src/test/java/com/aluracursos/dataDigimon/ApiDigimonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.dataDigimon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDigimonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
